9172d405915859f8a4a464f39432c813c002cad4
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-25 10:34:45 +0100

    added about

---
+++ react-library/src/pages/About.jsx
@@ -0,0 +1,29 @@
+export default function About() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="/home">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/">About</a>
+                    <a href="/contacts">Contacts</a>
+                </nav>
+            </header>
+
+            <main>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus tempore nesciunt cum! Assumenda officia doloribus
+                    repudiandae ea asperiores ducimus veniam ex, non aliquid
+                    itaque? Possimus fugit odio omnis voluptates nulla.</p>
+            </main>
+
+            <footer>
+                <p>copywrite 2024</p>
+            </footer>
+        </>
+    )
+}
