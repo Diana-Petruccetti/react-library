eff7fb4484f1867ad6fafe165649943f1d7943a5
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-11-25 10:36:24 +0100

    added contacts

---
+++ react-library/src/pages/Contacts.jsx
@@ -0,0 +1,29 @@
+export default function Contacts() {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <a href="/home">Home</a>
+                    <a href="/books">Books</a>
+                    <a href="/about">About</a>
+                    <a href="/">Contacts</a>
+                </nav>
+            </header>
+
+            <main>
+                <h1>Contacts</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusamus tempore nesciunt cum! Assumenda officia doloribus
+                    repudiandae ea asperiores ducimus veniam ex, non aliquid
+                    itaque? Possimus fugit odio omnis voluptates nulla.</p>
+            </main>
+
+            <footer>
+                <p>copywrite 2024</p>
+            </footer>
+        </>
+    )
+}
